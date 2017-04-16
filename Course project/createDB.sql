ed8226b32a31a033be87c4f2f81cfb9c453fb720
alex-kozinov <alex-kozinov@mail.ru>
2017-04-16 22:14:56 +0300

    add 3 create tables

--- Course project/createDB.sql
+++ Course project/createDB.sql
@@ -1,1 +1,20 @@
-;;:
+CREATE TABLE documents {
+    passport        VARCHAR(10) NOT NULL, 
+    faculty_code    INTEGER NOT NULL
+};
+CREATE TABLE faculty {
+    faculty_code    INTEGER PRIMARY KEY,
+    name            VARCHAR(100) NOT NULL,
+    university,     VARCHAR(100) NOT NULL,
+    city,           VARCHAR(100),
+    budget,         INTEGER NOT NULL,
+    subject1,       VARCHAR(20) NOT NULL,
+    subject2,       VARCHAR(20) NOT NULL,
+    subject3        VARCHAR(20) NOT NULL
+}
+CREATE TABLE subjects {
+    subject_code,   INTEGER NOT NULL UNIQUE
+    subject_name    VARCHAR(20) NOT NULL UNIQUE,
+    CONSTRAINT subj PRIMARY KEY(subject_code, subject_name)
+}
+
