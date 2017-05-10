d522b3379bd067d6252994adc095ca7eefb94804
nevstruev-dmitriy <nevstruev.dmitriy@mail.ru>
2017-05-10 19:25:59 +0300

    dimka

--- Course project/dimka.py
+++ Course project/dimka.py
@@ -1,29 +1,16 @@
 from bd_commands import *
 
-
-def townPoint(cursor):
-    cls()
-    print ("Введите город на русском: ")
-    in_ = input(">")
-    vuzInTown(cursor, in_)
-
-
-def vuzInTown(cursor, nameTown):
-    query_table(cursor, "SELECT university FROM Faculty WHERE city=?", (nameTown, ), "Dimka fail!")
+def universityInTown(cursor, nameTown):
+    query_table(cursor, "SELECT DISTINCT university FROM Faculty WHERE city=?", (nameTown, ), "Dimka fail!")
     cls()
     print_table(["университет"], cursor.fetchall())
     input("Нажмите клавишу для продолжения...")
 
-def universityPoint(cursor):
-	cls()
-	print ("Введите университет на русском: ")
-	in_ = input(">")
-	fakultetInUniver(cursor, in_)
-
-def fakultetInUniver(cursor, nameTown):
-    query_table(cursor, "SELECT name FROM Faculty WHERE university=?", (nameTown, ), "Dimka fail!")
+def fakultetInUniversity(cursor, nameTown, nameUniversity):
+    query_table(cursor, "SELECT DISTINCT name, faculty_code FROM Faculty WHERE university=? AND city=?", 
+				(nameUniversity, nameTown), "Dimka fail!")
     cls()
-    print_table(["факультет"], cursor.fetchall())
+    print_table(["факультет", "код факультета"], cursor.fetchall())
     input("Нажмите клавишу для продолжения...")
 
 
